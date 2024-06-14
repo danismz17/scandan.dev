5da8f2cc8f8994064514eae306d9a1986f06695c
danismz17 <daniel.scandroglio@gmail.com>
2024-06-14 00:46:16 -0300

    v1.0

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
     <>
       <div className="alert-container">
         <h1>Hola! estoy actualizando la página</h1>
-        <h2>quedará operativa a la brevedad</h2>
+        <h2>quedará operativa a la brevedad.</h2>
         <img src="/danscan.png" alt="" />
       </div>
     </>
